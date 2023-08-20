c156dbd3231c915d845c380f786f71735b9f97a1
LingLing <ryoung6920@gmail.com>
2023-08-21 00:28:01 +0900

    08/21 00:27 평균구하기 문제풀이

--- src/lv_1/약수의합.java
+++ src/lv_1/약수의합.java
@@ -4,30 +4,35 @@ import java.util.List;
 
 
 
-// 35:29 & 100점
+// 풀이시간 00:35:29 & 제출점수 100점
 
 
 
-class Solution {	
+public class 약수의합 {
 	
-    public int solution(int n) {
-        
-        int answer = 0;
-        List<Integer> divisor = new ArrayList<Integer>();
-        
-        // 나누었을 때 나머지가 0이 되는 것 == 약수
-        for(int i = 1; i <= n; i++){
-            if(n % i == 0){
-            	divisor.add(i);
-            }
-        }
-        
-        // 약수의 누적합 구하기
-        for(int num : divisor) {
-			answer += num;
-		}
-        
-        return answer;
-        
-    }
+	class Solution {	
+		
+	    public int solution(int n) {
+	        
+	        int answer = 0;
+	        List<Integer> divisor = new ArrayList<Integer>();
+	        
+	        // 나누었을 때 나머지가 0이 되는 것 == 약수
+	        for(int i = 1; i <= n; i++){
+	            if(n % i == 0){
+	            	divisor.add(i);
+	            }
+	        }
+	        
+	        // 약수의 누적합 구하기
+	        for(int num : divisor) {
+				answer += num;
+			}
+	        
+	        return answer;
+	        
+	    }
+	    
+	} // solution
+	
 }
