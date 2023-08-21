4d9c4a25a637097ea738d65b4da7cf001306525d
LingLing <ryoung6920@gmail.com>
2023-08-21 22:53:46 +0900

    23/08/21 22:53 자연수 뒤집어 배열로 만들기 문제풀이

--- Lv_1/자연수뒤집어배열로만들기.java
+++ Lv_1/자연수뒤집어배열로만들기.java
@@ -1,13 +1,29 @@
+import java.util.*;
 
 
 
-
-// 풀이시간 01:03:25 & 제출점수 100점
+// 풀이시간 00:08:11 & 제출점수 100점
 
 
 
 public class 자연수뒤집어배열로만들기 {
 
-	
+	class Solution {
+	    
+		public List<Long> solution(long n) {
+	        
+	        List<Long> list = new ArrayList<Long>();
+			
+	        while(n > 0) {
+	        	long tmp = n % 10;	// 나머지
+	        	list.add(tmp);
+	        	n /= 10;            // 몫
+	        }
+	        
+	        return list;
+	        
+	    }
+		
+	} // Solution
 	
 }
