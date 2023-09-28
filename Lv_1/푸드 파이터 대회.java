b6bca027780ab48e795e029d37f9ea1bcf203076
LingLing <131628980+TAE-RYOUNG-ING@users.noreply.github.com>
2023-09-28 20:36:46 +0900

    230928 / PGM / Lv푸드 파이터 대회.java

---
+++ Lv_1/푸드 파이터 대회.java
@@ -0,0 +1,19 @@
+
+// 풀이시간 15:12
+
+
+class Solution {
+  public String solution(int[] food) {
+  
+    StringBuilder sb = new StringBuilder();
+  
+    for(int i=1; i<food.length; i++) {
+      int tmp = food[i]/2;
+      for(int j=0; j<tmp; j++) {
+        sb.append(i);
+      }
+    }
+  
+    return sb.toString() + "0" + sb.reverse().toString();
+  }
+}
