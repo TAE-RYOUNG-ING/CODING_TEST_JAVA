42a262da2437685e66c80a28594b83697aa46a7c
LingLing <131628980+TAE-RYOUNG-ING@users.noreply.github.com>
2023-10-03 13:32:43 +0900

    231003 / PGM / Lv_1 / 명예의 전당(1)_update.java

--- Lv_1/명예의 전당(1).java
+++ Lv_1/명예의 전당(1).java
@@ -3,23 +3,23 @@
 
 
 class Solution {
-  public int[] solution(int k, int[] score) {
-      
-	  List<Integer> goat = new ArrayList<>();
+	public int[] solution(int k, int[] score) {
+	      
+		List<Integer> goat = new ArrayList<>();
 		int[] answer = new int[score.length];
-	
+		
 		// k가 score 길이보다 클 경우
-	  if(k > score.length) {
+		if(k > score.length) {
 			k = score.length;
 		}
-	      
-	  // k일까지의 발표점수
+		      
+		// k일까지의 발표점수
 		for(int i=0; i<k; i++) {
 			goat.add(score[i]);
 			Collections.sort(goat);
 			answer[i] = goat.get(0);
 		}
-	
+		
 		// k+1일부터 발표점수
 		for(int i=k; i<score.length; i++) {
 			if(goat.size() > k) {
@@ -29,7 +29,7 @@ class Solution {
 			Collections.sort(goat, Collections.reverseOrder());
 			answer[i] = goat.get(k-1);
 		}
-
-    return answer;
-  }
+	
+		return answer;
+	}
 }
