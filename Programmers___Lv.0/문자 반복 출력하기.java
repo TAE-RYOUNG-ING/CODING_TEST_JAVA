a629f9cbe16ec477799965e7b46187e502c25f15
LingLing <131628980+TAE-RYOUNG-ING@users.noreply.github.com>
2023-10-05 13:34:04 +0900

    231005 / PGM / Lv_0 / 문자 반복 출력하기.java

---
+++ Programmers___Lv.0/문자 반복 출력하기.java
@@ -0,0 +1,15 @@
+
+// 풀이시간 03:47
+
+
+class Solution {
+  public String solution(String my_string, int n) {
+  
+    StringBuilder sb = new StringBuilder();
+    for(char ch : my_string.toCharArray()){
+      sb.append((ch + "").repeat(n));
+    }
+  
+    return sb.toString();
+  }
+}
