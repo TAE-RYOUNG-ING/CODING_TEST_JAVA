6dc7fd3c592239d7a74d56ba5ed2d404774f8099
LingLing <131628980+TAE-RYOUNG-ING@users.noreply.github.com>
2023-10-06 22:42:48 +0900

    231006 / PGM / Lv_1 / 카드 뭉치.java

---
+++ Programmers___Lv.1/카드 뭉치.java
@@ -0,0 +1,25 @@
+
+// 풀이시간 40:22
+
+
+class Solution {
+  public String solution(String[] cards1, String[] cards2, String[] goal) {
+    
+      int idx1 = 0, idx2 = 0;
+    
+      for(int i=0; i<goal.length; i++){
+          String target = goal[i];
+          if(idx1 < cards1.length && cards1[idx1].equals(target)){
+              idx1++;
+          }
+          else if(idx2 < cards2.length && cards2[idx2].equals(target)){
+              idx2++;
+          }
+          else{
+              return "No";
+          }
+      }
+
+      return "Yes";
+    }
+}
