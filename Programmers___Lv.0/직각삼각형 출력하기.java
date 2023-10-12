e247c3b6fd9211f3618aac0797072b8e03ce15e8
LingLing <131628980+TAE-RYOUNG-ING@users.noreply.github.com>
2023-10-12 15:11:51 +0900

    231012 / PGM / Lv_0 / 직각삼각형 출력하기.java

---
+++ Programmers___Lv.0/직각삼각형 출력하기.java
@@ -0,0 +1,15 @@
+// 풀이시간 05:23
+
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    String star = "*";
+    for(int i=1; i<=n; i++) {
+      System.out.println(star);
+      star += "*";
+    }
+  }
+}
